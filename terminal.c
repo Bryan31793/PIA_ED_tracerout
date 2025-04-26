f1652265a974678c66aa58b51f46670d7a0bf3c7
Bryan <bryanrmzlop@gmail.com>
2025-04-25 20:16:28 -0600

    Feature: mejora en la respuesta a espacios y tabulaciones

--- terminal.c
+++ terminal.c
@@ -1,31 +1,80 @@
 //aqui va todo el codigo que involucra a la terminal
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
 
 //funcion que se encarga de llamar a todas las demas
 void main_terminal();
 
+//funcion que se encarga de validar la entrada
+char *leer_entrada();
+
 void main_terminal()
 {
-    char aux[50];
 
     while(1)
     {   
         printf(">> ");
+        char *aux = NULL;
+        aux = leer_entrada();
 
-        fgets(aux, sizeof(aux), stdin);
-        aux[strcspn(aux, "\n")] = '\0'; //strcspn regresa la cantidad de caracteres que no contienen "\n" (debe pasarse como cadena)
+        if(aux) {
+            if(strcmp(aux, "saludo") == 0)
+            printf("\n\tHola colega!!\n");
+            else if(strcmp(aux, "salir") == 0) {
+                printf("\n\tSaliendo...\n");
+                break;
+            } 
+            else if(aux[0] != '\0')     //esta es la ultima condicion
+                printf("\n\tError: no existe comando: '%s'\n", aux);   
+            
+            free(aux);
+        }
 
-        if(strcmp(aux, "saludo") == 0)
-            printf("\n\tHola colega!!");
-        else if(strcmp(aux, "salir") == 0) {
-            printf("\n\tSaliendo...");
-            break;
-        }    
+        
+    }
+}
+
+char *leer_entrada()     //el comando se podria leer dentro de esta funcion
+{
+    //debo crear un bloque de memoria
+    char *entrada = (char*)malloc(sizeof(char) * 50);
+    if(!entrada) {
+        printf("\n\tError al asignar memoria");
+        return NULL;
+    }
         
 
-        //printf("\n");   //podria ir junto con el primer printf
+    fgets(entrada, 50, stdin);
+    entrada[strcspn(entrada, "\n")] = '\0'; //strcspn regresa la cantidad de caracteres que van desde aux[0] a "\n" (debe pasarse como cadena)
+
+    int i = 0;
+    while(entrada[i] == ' ' || entrada[i] == '\t') {
+        i++;
     }
+
+    if(entrada[i] == '\0') { //la cadena unicamente contiene espacios o tabulaciones
+        entrada[0] = '\0';  
+        return entrada;
+    }
+
+    char *comando = (char*)malloc(sizeof(char) * 50);
+    if(!comando) {
+        printf("\n\tError al asignar memoria");
+        return NULL;
+    }
+    int j = 0;
+
+    while(entrada[i] != ' ' && entrada[i] != '\0') {
+        comando[j] = entrada[i];
+        j++;
+        i++;
+    }
+    free(entrada);
+    comando[j] = '\0';
+    return comando;
+    
 }
 
 int main()
